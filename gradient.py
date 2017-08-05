b1bc7d62a14c667f478147be8f009758a0057490
privetpoka <fififinger@gmail.com>
2017-08-05 20:26:23 +0300

    prepare gradien for manager

--- gradient.py
+++ gradient.py
@@ -4,7 +4,6 @@ class LinRegression:
     def __init__(self):
         self.about = "Gradient descent"
         self.W = [] # model's weights
-        self.is_scalling = False # is feature scaling used
         
     def cost_function(self, y_real, y_pred): 
         # cost function for gradient descent algorithm
@@ -15,25 +14,14 @@ class LinRegression:
         s = (np.dot(dy.T, X_tr)).reshape(n, 1)
         dW = 2*(learning_rate*s/m).reshape(n, 1)
         return self.W - dW
-    
-    def normalize(self, X):
-        # normilize X table
-        for j in range(X.shape[1]):
-            X[:,j] = X[:,j]/np.max(X[:,j])
-        return X
+
     
     def fit(self, X, y, learning_rate = 0.99, nsteps = 3000, e = 0.000000001,
-            weight_low = 0, weight_high = 1,
-            is_scalling = False):
+            weight_low = 0, weight_high = 1):
         # train our Linear Regression model
         
         np.random.seed(0)
         X = X.astype(float)
-        
-        # Normilize process
-        if is_scalling == True:
-            X = self.normalize(X)
-            self.is_scalling = True
         m = X.shape[0]
         # add one's column to X
         X = np.hstack( (np.ones(m).reshape(m, 1), X) )
@@ -68,14 +56,9 @@ class LinRegression:
     
     def predict(self, X):
         m = X.shape[0]
-        if self.is_scalling == False:
-            return np.dot( np.hstack( (np.ones(m).reshape(m, 1),
-                                       X.astype(float)) ) ,
-                          self.W)
-        else:
-            return np.dot( np.hstack( (np.ones(m).reshape(m, 1),
-                                       self.normalize(X.astype(float))) ),
-                          self.W)
+        
+        return np.dot( np.hstack( (np.ones(m).reshape(m, 1),X.astype(float)) ) ,self.W)
+
 
 f = open ('./data_set.csv','r')
 x =  np.loadtxt(f,skiprows=1,delimiter=',',usecols=(1,2,4))
@@ -86,4 +69,6 @@ lr.fit(x,y,learning_rate=0.0001,nsteps=10000)
 print(lr.W)
 f = open ('./data_set_check.csv','r')
 data_set_for_check = np.loadtxt(f,delimiter=',',skiprows=1,usecols=(1,2,4))
-print (lr.predict(data_set_for_check))
+f = open ('./target_check.csv')
+check = np.loadtxt(f,delimiter=',',skiprows=1,usecols=1)
+
