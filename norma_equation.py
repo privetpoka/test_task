93566fd16ffbb0be06ffcdf3aedacb0a99016eed
privetpoka <fififinger@gmail.com>
2017-08-05 20:25:38 +0300

    adding manager

---
+++ norma_equation.py
@@ -0,0 +1,17 @@
+import numpy as np
+class Normal_Equation():
+	"""docstring for ClassName"""
+	def __init__(self):
+		self.about = "Normal equation"
+		self.W=[]
+
+	def evaluate(self,x,y):
+		m = x.shape[1]
+		one=np.ones((m,1))
+		complete=np.hstack((one,x))
+		self.W = np.linalg.inv(complete.T.dot(complete)).dot(complete.T).dot(y)
+		return self.W
+	def predict (self, target):
+		m = target.shape[1]
+		one = np.ones((m,1))
+		return np.hstack((one,target)).dot(self.W)
