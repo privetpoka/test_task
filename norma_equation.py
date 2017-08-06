6e45f8222bf26031d551734c3827d43662e0fa01
privetpoka <fififinger@gmail.com>
2017-08-06 03:07:52 +0300

    just to commit

--- norma_equation.py
+++ norma_equation.py
@@ -3,15 +3,15 @@ class Normal_Equation():
 	"""docstring for ClassName"""
 	def __init__(self):
 		self.about = "Normal equation"
-		self.W=[]
+		self.theta=[]
 
 	def evaluate(self,x,y):
 		m = x.shape[0]
 		one=np.ones((m,1))
 		complete=np.hstack((one,x))
-		self.W = np.linalg.inv(complete.T.dot(complete)).dot(complete.T).dot(y)
-		return self.W
+		self.theta = np.linalg.inv(complete.T.dot(complete)).dot(complete.T).dot(y)
+		return self.theta
 	def predict (self, target):
 		m = target.shape[0]
 		one = np.ones((m,1))
-		return np.hstack((one,target)).dot(self.W)
+		return np.hstack((one,target)).dot(self.theta)
